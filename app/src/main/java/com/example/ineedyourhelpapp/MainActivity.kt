898d1ae8c8c87bef3ca717b593e82a24bdc6d0e6
danielinsubria <dpedrotti@studenti.uninsubria.it>
2021-04-22 16:00:11 +0200

    Prima stesura

--- app/src/main/java/com/example/ineedyourhelpapp/MainActivity.kt
+++ app/src/main/java/com/example/ineedyourhelpapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.ineedyourhelpapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun OpenSecondActivity(v:View) {
+        val intent= Intent(this@MainActivity, MainActivity2::class.java)
+        startActivity(intent)
     }
 }
