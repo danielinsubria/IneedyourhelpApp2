33707747661244a7bd71b97664c1b790e82d4b6a
danielinsubria <dpedrotti@studenti.uninsubria.it>
2021-05-11 15:00:10 +0200

    aggiornamento pulsanti db

--- app/src/main/java/com/example/ineedyourhelpapp/MainActivity2.kt
+++ app/src/main/java/com/example/ineedyourhelpapp/MainActivity2.kt
@@ -3,13 +3,13 @@ package com.example.ineedyourhelpapp
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
 
-    lateinit var usersDBHelper : UsersDBHelper
+    lateinit var usersDBHelper: UsersDBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,26 +18,38 @@ class MainActivity2 : AppCompatActivity() {
         usersDBHelper = UsersDBHelper(this)
     }
 
-    fun addUser(v:View){
+    fun addUser(v: View) {
         var name = this.edittext_name.text.toString()
         var number = this.edittext_number.text.toString()
-        var result = usersDBHelper.insertUser(UserModel(name = name, number = number))
+        if ((name != null && name.length > 2) && (number != null && number.length > 2)) {
+            Toast.makeText(this@MainActivity2, "Utente aggiunto", Toast.LENGTH_SHORT).show()
+            var result = usersDBHelper.insertUser(UserModel(name = name, number = number))
+        } else {
+            Toast.makeText(this@MainActivity2, "nome o numero non valido", Toast.LENGTH_SHORT).show()
+        }
 
         //clear all edittext s
         this.edittext_number.setText("")
         this.edittext_name.setText("")
-        this.textview_result.text = "Added user "
+        //this.textview_result.text = "Added user "
         this.ll_entries.removeAllViews()
+
+        showAllUsers(v)
+
     }
 
-    fun deleteUser(v:View){
+    fun deleteUser(v: View) {
         var name = this.edittext_name.text.toString()
-        val result = usersDBHelper.deleteUser(name)
-        this.textview_result.text = "Deleted user"
+        if (name != null && name.length > 2) {
+            val result = usersDBHelper.deleteUser(name)
+            this.textview_result.text = "Deleted user"
+        } else {
+            this.textview_result.text = "Inserire il nome "
+        }
         this.ll_entries.removeAllViews()
     }
 
-    fun showAllUsers(v:View){
+    fun showAllUsers(v: View) {
         var users = usersDBHelper.readAllUsers()
         this.ll_entries.removeAllViews()
         users.forEach {
@@ -49,3 +61,4 @@ class MainActivity2 : AppCompatActivity() {
         this.textview_result.text = "Find " + users.size + " users"
     }
 }
+
